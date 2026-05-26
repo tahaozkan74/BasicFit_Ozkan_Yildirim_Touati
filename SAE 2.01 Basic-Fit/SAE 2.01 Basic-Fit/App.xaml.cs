6d2e4c0b6ed5c7d24908421de959cb5ff277c3d2
tahaozkan74 <taha.ozkan@etu.univ-smb.fr>
2026-05-26 19:58:32 +0200

    Add files via upload

---
+++ SAE 2.01 Basic-Fit/SAE 2.01 Basic-Fit/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SAE_2._01_Basic_Fit
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
