f7ef6bd3273eb020d804882a36a4fe4fe2085ee6
ElvisAndr <Elvis-Aaron.Andrianantoandro@etu.univ-smb.fr>
2026-06-02 09:33:54 +0200

    Mise a jour des classes Final (avec ajout employe)

--- SAE 2.01 Basic-Fit/SAE 2.01 Basic-Fit/obj/Debug/net8.0-windows/App.g.i.cs
+++ SAE 2.01 Basic-Fit/SAE 2.01 Basic-Fit/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace SAE_2._01_Basic_Fit {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.6.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace SAE_2._01_Basic_Fit {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.6.0")]
         public static void Main() {
             SAE_2._01_Basic_Fit.App app = new SAE_2._01_Basic_Fit.App();
             app.InitializeComponent();
